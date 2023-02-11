b0f28156ba00de96eaca09e7a6c6260c47ec1c4b
zkz098 <zkz20081204@126.com>
2023-02-11 22:06:36 +0800

    chore(*): 杂项更改

--- source/js/_app/library.ts
+++ source/js/_app/library.ts
@@ -4,13 +4,6 @@
 对注释的说明: 部分注释为openai-chatgpt生成的注释,可能存在描述或语义的问题
  */
 
-/*!
-This file is part of Hexo-theme-shokaX.
-Hexo-theme-shokaX is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Hexo-theme-shokaX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
- */
-
 declare interface EventTarget {
 // 设置或获取元素属性值
   attr(type: string, value?: any): any
