fedae123e7588b900909bfbef79ee27880972b2a
zkz098 <zkz20081204@126.com>
2023-02-12 12:29:08 +0800

    feat(script): 自动检查版本更新

--- scripts/plugin/index.js
+++ scripts/plugin/index.js
@@ -4,3 +4,46 @@ hexo.on('generateBefore', () => {
   // 加载`theme_injects`过滤器
   require('./lib/injects')(hexo)
 })
+
+hexo.on('generateAfter', () => {
+  // 检查版本更新
+  const https = require('https')
+  const path = require('path')
+  const { version } = require(path.normalize('../../package.json'))
+  https.get('https://api.github.com/repos/zkz098/hexo-theme-shokaX/releases/latest', {
+    headers: {
+      'User-Agent': 'Theme ShokaX Client'
+    }
+  }, (res) => {
+    let result = ''
+    res.on('data', (data) => {
+      result += data
+    })
+    res.on('end', () => {
+      try {
+        const latest = JSON.parse(result).tag_name.replace('v', '').split('.')
+        const current = version.split('.')
+        let isOutdated = false
+        for (let i = 0; i < Math.max(latest.length, current.length); i++) {
+          if (!current[i] || latest[i] > current[i]) {
+            isOutdated = true
+            break
+          }
+          if (latest[i] < current[i]) {
+            break
+          }
+        }
+        if (isOutdated) {
+          hexo.log.warn(`Your theme ShokaX is outdated. Current version: v${current.join('.')}, latest version: v${latest.join('.')}`)
+          hexo.log.warn('Visit https://github.com/zkz098/hexo-theme-shokaX/releases for more information.')
+        }
+      } catch (err) {
+        hexo.log.error('Failed to detect version info. Error message:')
+        hexo.log.error(err)
+      }
+    })
+  }).on('error', err => {
+    hexo.log.error('Failed to detect version info. Error message:')
+    hexo.log.error(err)
+  })
+})
