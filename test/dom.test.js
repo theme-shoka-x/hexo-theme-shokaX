1cfcdb67de1bd819bd6d86f2a234263b92868ad7
zkz098 <zkz20081204@126.com>
2023-08-05 12:41:59 +0800

    ci: 更新qodana配置文件 & test: 增加单元测试

--- test/dom.test.js
+++ test/dom.test.js
@@ -46,3 +46,41 @@ describe('$dom', function () {
     expect(element).to.be.null
   })
 })
+
+describe('$dom.all & $dom.each', function () {
+  beforeEach(function () {
+    const html = `
+      <html lang="zh-cn">
+        <head><title>Test Page</title></head>
+        <body>
+          <div id="container">
+            <p class="text">Hello</p>
+            <p class="text">World</p>
+          </div>
+        </body>
+      </html>
+    `
+    const dom = new jsdom.JSDOM(html)
+    global.document = dom.window.document
+    global.HTMLElement = dom.window.HTMLElement
+  })
+
+  afterEach(function () {
+    global.document = null
+  })
+
+  it('should return all elements with the given selector', () => {
+    const elements = $dom.all('p')
+    expect(elements).to.be.exist
+    expect(elements).length(2)
+  })
+
+  it('should run callback with the given selector', () => {
+    $dom.each('p', function (value){
+      value.innerText = 'test'
+    })
+    const elements = $dom.all('p')
+    expect(elements[0].innerText).equal('test')
+    expect(elements[1].innerText).equal('test')
+  })
+})
