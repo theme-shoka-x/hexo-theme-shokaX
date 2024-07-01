bda015c069f05c5f9e058d94ac97b7313fb3a5f6
zkz098 <zkz20081204@126.com>
2024-07-01 19:31:33 +0800

    fix: 自动跳过依赖提升

--- toolbox/compiler.mjs
+++ toolbox/compiler.mjs
@@ -61,11 +61,15 @@ if (CONFIG.legacyScript) {
 }
 
 if (CONFIG.depsHoist) {
-    const deps = JSON.parse(await fs.readFile(path.join(hexoRoot,'package.json').trim(), 'utf-8')).dependencies
-    const depsList = Object.keys(deps).map(d => `${d}@${deps[d]}`)
-    console.log('Hoisting dependencies...')
-    await execShell(`pnpm add ${depsList.join(' ')}`.trim())
-    console.log('Finished hoisting dependencies.')
+    try {
+        const deps = JSON.parse(await fs.readFile(path.join(hexoRoot, 'package.json').trim(), 'utf-8')).dependencies
+        const depsList = Object.keys(deps).map(d => `${d}@${deps[d]}`)
+        console.log('Hoisting dependencies...')
+        await execShell(`pnpm add ${depsList.join(' ')}`.trim())
+        console.log('Finished hoisting dependencies.')
+    } catch (e) {
+        console.log('Skipping hoisting dependencies.')
+    }
 }
 
 console.log('Done.')
