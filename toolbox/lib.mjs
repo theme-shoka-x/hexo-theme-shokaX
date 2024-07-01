b04646518c42c62c1556e32f53d32f64b212440a
zkz098 <zkz20081204@126.com>
2024-07-01 20:04:13 +0800

    feat: 允许 toolbox 自动检测包管理器

--- toolbox/lib.mjs
+++ toolbox/lib.mjs
@@ -12,12 +12,28 @@ if (hexoRoot.startsWith('file:/')) {
   hexoRoot = hexoRoot.slice(8); // 去除 'file:\'
 }
 
+async function checkFileAccessible(file) {
+  try {
+    await fs.access(file)
+  } catch {
+    return false
+  }
+  return true
+}
+
 export async function hoistDeps() {
+  let pm
+  if (await checkFileAccessible('pnpm-lock.yml') || await checkFileAccessible('pnpm-lock.yaml') || await checkFileAccessible('enable_pnpm')) {
+    pm = "pnpm add"
+  } else if (await checkFileAccessible('yarn.lock') || await checkFileAccessible('.yarnrc.yml')) {
+    pm = "yarn add"
+  } else {
+    pm = "npm install"
+  }
   try {
     const deps = JSON.parse(await fs.readFile(path.join(hexoRoot, 'package.json').trim(), 'utf-8')).dependencies
     const depsList = Object.keys(deps).map(d => `${d}@${deps[d]}`)
-    console.log('Hoisting dependencies...')
-    await execShell(`pnpm add ${depsList.join(' ')}`.trim())
+    await execShell(`${pm} ${depsList.join(' ')}`.trim())
   } catch (e) {
     console.log('Skipping hoisting dependencies.')
   }
