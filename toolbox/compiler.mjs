22655afc8ec82dc454cc24348a686d35c1332aca
zkz098 <zkz20081204@126.com>
2024-07-01 18:19:20 +0800

    feat: toolbox

---
+++ toolbox/compiler.mjs
@@ -0,0 +1,65 @@
+/*
+ShokaX ToolBox - Compiler
+compatibility: ShokaX v0.4.x
+ */
+import path from "node:path";
+import { promisify } from "node:util";
+import fs from 'fs/promises'
+
+const CONFIG = {
+  depsHoist: true,
+  minify: false,
+  legacyScript: true,
+  pm: 'pnpm add' // or yarn add / npm install
+}
+
+async function deleteFileRecursive(dir) {
+  const files = await fs.readdir(dir)
+  for (const f of files) {
+      const fullPath = path.join(dir, f)
+      const stat = await fs.lstat(fullPath)
+      if (stat.isDirectory()) {
+        await deleteFileRecursive(fullPath)
+      } else {
+        const fileExt = path.extname(f)
+        if (fileExt === '.ts' || fileExt === '.json' || fileExt === '.tsbuildinfo') {
+          await fs.unlink(fullPath)
+        }
+      }
+  }
+}
+
+const execShell = promisify(await import('child_process').exec)
+
+console.log('ShokaX ToolBox - Compiler')
+console.log('Installing compiler dependencies...')
+
+await execShell(`${CONFIG.pm} typescript esbuild -g`)
+
+console.log('Start compiling...')
+
+const hexoRoot = path.join(import.meta.url, './../../')
+if (CONFIG.legacyScript) {
+    console.log('Simulating legacy script compiler...')
+    const sPath = path.join(hexoRoot, 'scripts/')
+    await execShell(`cd ${sPath} && tsc --build`)
+    console.log('Deleting typescript files...')
+    await deleteFileRecursive(sPath)
+    console.log('Finished compiling.')
+} else {
+    throw Error('Not implemented yet.')
+}
+
+if (CONFIG.depsHoist) {
+    const deps = JSON.parse(await fs.readFile(path.join(hexoRoot,'package.json'), 'utf-8')).dependencies
+    const depsList = Object.keys(deps).map(d => `${d}@${deps[d]}`)
+    console.log('Hoisting dependencies...')
+    await execShell(`${CONFIG.pm} ${depsList.join(' ')}`)
+    console.log('Finished hoisting dependencies.')
+}
+
+console.log('Done.')
+
+
+
+
