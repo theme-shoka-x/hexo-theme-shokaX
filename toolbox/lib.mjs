a0aad3c076e110b6c16d6d28303db1a4b3768ffe
zkz098 <zkz20081204@126.com>
2024-07-01 19:47:24 +0800

    fix: toolbox

--- toolbox/lib.mjs
+++ toolbox/lib.mjs
@@ -5,6 +5,13 @@ import child_process from "child_process";
 
 const execShell = promisify(child_process.exec)
 
+let hexoRoot = path.join(import.meta.url, './../../../../').trim()
+if (hexoRoot.startsWith('file:/')) {
+  hexoRoot = hexoRoot.slice(5); // 去除 'file://'
+} else if (hexoRoot.startsWith('file:\\')) {
+  hexoRoot = hexoRoot.slice(8); // 去除 'file:\'
+}
+
 export async function hoistDeps() {
   const deps = JSON.parse(await fs.readFile(path.join(hexoRoot, 'package.json').trim(), 'utf-8')).dependencies
   const depsList = Object.keys(deps).map(d => `${d}@${deps[d]}`)
