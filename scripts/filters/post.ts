1951a3e1fab4396d607fbb057d8cb3486d69181f
四散的尘埃 <3198312396@qq.com>
2025-02-15 17:04:00 +0800

    Update submodule content

--- scripts/filters/post.ts
+++
@@ -1,6 +0,0 @@
-/* global hexo */
-
-hexo.extend.filter.register('after_post_render', (data) => {
-  // 使用正则表达式将<img>标签的src属性替换为data-src属性
-  data.content = data.content.replace(/(<img[^>]*) src=/img, '$1 loading="lazy" data-src=')
-}, 0)
