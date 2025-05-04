23d754fa38d44c9911570cc1b84828d0ed926e69
zkz098 <zkz20081204@126.com>
2025-05-04 12:21:35 +0800

    fix: 修复img加载错误

--- source/js/_app/page/imageviewer.ts
+++ source/js/_app/page/imageviewer.ts
@@ -1,11 +1,10 @@
-import DOMPurify from 'dompurify';
 import { HanaImgViewer } from 'hana-img-viewer'
 import { createApp } from 'vue';
 
 export const postImageViewer = (p: string) => {
   document.querySelectorAll(`${p} .md img:not(.emoji):not(.vemoji)`).forEach((element) => {
     const img = element as HTMLImageElement;
-    const imgSrc = DOMPurify.sanitize(img.dataset.src);
+    const imgSrc = img.src
 
     const wrapper = document.createElement('div');
     img.replaceWith(wrapper);
